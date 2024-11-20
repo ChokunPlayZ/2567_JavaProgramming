83e7ca9d53fd62b4cfa912d7be04fb6b2fb706e6
Chokun Rojanapron <chokunplayz@ckl.moe>
2024-11-20 10:19:56 +0700

    add file back, add dialog

---
+++ FirstProgram.java
@@ -0,0 +1,13 @@
+class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("StudentId: 2413110624");
+        System.out.println("StudentName:Yanavut Rojanapron");
+        System.out.println("Topic:Introduction to Java Programming");
+        System.out.println("Date:11/20/2024");
+
+        JOptionPane.showMessageDialog(null,
+                "StudentId: 2413110624\n"+"StudentName:Yanavut Rojanapron\n"+"Topic:Introduction to Java Programming\n"+"Date:11/20/2024",
+                "FirstProgram",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+}
