8259865871f860c7af31b684260669ae414af6b8
Chokun Rojanapron <chokunplayz@ckl.moe>
2024-11-20 10:12:50 +0700

    first program

---
+++ FirstProgram.java
@@ -0,0 +1,8 @@
+class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("StudentId: 2413110624");
+        System.out.println("StudentName:Yanavut Rojanapron");
+        System.out.println("Topic:Introduction to Java Programming");
+        System.out.println("Date:11/20/2024")
+    }
+}
