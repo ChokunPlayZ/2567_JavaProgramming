a7f446d065143f7579364a6142e13dd2981d8f14
Chokun Rojanapron <chokunplayz@ckl.moe>
2025-02-19 08:28:18 +0700

    Lab9 Homework

---
+++ src/Lab9/PattanakarnBranch.java
@@ -0,0 +1,20 @@
+package Lab9;
+
+public class PattanakarnBranch extends Product{
+    public int payUnit() {
+        return super.getUnit() / 2;
+    }
+
+    public int freeUnit() {
+        return super.getUnit() / 2;
+    }
+
+
+    public int getTotalPrice() {
+        return (super.getUnit() / 2) * 100;
+    }
+
+    public String toString() {
+        return String.format("You buy %s units, get free %s units (%s)", this.payUnit(), this.freeUnit(), getTotalPrice());
+    }
+}
---
+++ src/Lab9/Product.java
@@ -0,0 +1,21 @@
+package Lab9;
+
+public class Product {
+    private int unit;
+
+    public void setUnit(int unit) {
+        this.unit = unit;
+    }
+
+    public int getUnit() {
+        return this.unit;
+    }
+
+    public int getTotalPrice() {
+        return getUnit() * 100;
+    }
+
+    public String toString() {
+        return String.format("You buy %s units (%s).", getUnit(), getTotalPrice());
+    }
+}
